9b7e070d411996c26c395536729558b09a4902c2
Bar <baryed55gmail.com>
2020-10-04 17:48:35 +0300

    basic works really cool

--- src/App/index.ts
+++ src/App/index.ts
@@ -2,6 +2,7 @@ import chalk from 'chalk';
 import { functions } from 'Common';
 import { clear } from 'console';
 import figlet from 'figlet';
+import { createFolder } from 'Functions';
 import inquirerFunction from 'Lib/inquirerSetupFunction';
 
 clear();
@@ -9,11 +10,11 @@ clear();
 console.log(chalk.blue(figlet.textSync('Bar Base Project', { horizontalLayout: 'full' })));
 const run = async () => {
 	const { projectName } = await inquirerFunction.projectName();
+	await createFolder(projectName);
 	const result = await inquirerFunction.projectSetup();
 	Object.values(result).forEach((value: string) => {
-		functions[value]();
+		functions[value](projectName);
 	});
-	console.log(`Project Name: ${projectName}`);
 };
 
 run();
--- src/Functions/index.ts
+++ src/Functions/index.ts
@@ -1,31 +1,53 @@
-const sharedFolders = () => {
+import ncp from 'ncp';
+import mkdirp from 'mkdirp';
+
+const baseUrl = './src';
+
+export const createFolder = async (name: string) => {
+	await mkdirp(`${baseUrl}/Export/${name}`);
+	console.log('created project folder');
+};
+
+const copyFolders = (source: string, destination: string) => {
+	ncp(source, destination, (err) => {
+		if (err) {
+			return console.error(err);
+		}
+	});
+};
+
+const sharedFolders = (projectName: string) => {
+	copyFolders(`${baseUrl}/Resources/Shared`, `${baseUrl}/Export/${projectName}`);
 	console.log('sharedFolders');
 };
 
-export const normalReact = () => {
-	console.log('normalReact');
+export const normalReact = (projectName: string) => {
+	sharedFolders(projectName);
+	copyFolders(`${baseUrl}/Resources/Normal React`, `${baseUrl}/Export/${projectName}`);
 };
 
-export const nextJs = () => {
-	console.log('nextJs');
+export const nextJs = (projectName: string) => {
+	sharedFolders(projectName);
+	copyFolders(`${baseUrl}/Resources/Next`, `${baseUrl}/Export/${projectName}`);
 };
 
-export const normalStructure = () => {
-	console.log('normalStructure');
+export const normalStructure = (projectName: string) => {
+	console.log(`normalStructure TEMP ${projectName}`);
+	// copyFolders(`${baseUrl}/Resources/Normal React`, `${baseUrl}/Export/${projectName}`);
 };
 
-export const withServer = () => {
-	console.log('withServer');
+export const withServer = (projectName: string) => {
+	copyFolders(`${baseUrl}/Resources/Server`, `${baseUrl}/Export/${projectName}-server`);
 };
 
-export const monorepo = () => {
-	console.log('monorepo');
+export const monorepo = (projectName: string) => {
+	copyFolders(`${baseUrl}/Resources/Monorepo`, `${baseUrl}/Export/${projectName}`);
 };
 
-export const mobx = () => {
-	console.log('mobx');
+export const mobx = (projectName: string) => {
+	copyFolders(`${baseUrl}/Resources/Mobx`, `${baseUrl}/Export/${projectName}`);
 };
 
-export const apollo = () => {
-	console.log('apollo');
+export const apollo = (projectName: string) => {
+	copyFolders(`${baseUrl}/Resources/Apollo`, `${baseUrl}/Export/${projectName}`);
 };
