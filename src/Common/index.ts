45abf24edf0985ea288af82ea3445d50e03bc167
Bar <baryed55gmail.com>
2020-11-27 16:54:54 +0200

    commit

--- src/Common/index.ts
+++ src/Common/index.ts
@@ -13,12 +13,12 @@ import {
 export const NORAML_REACT = 'normal react';
 export const NEXT_JS = 'next.js';
 export const NORMAL_STRUCTURE = 'normal';
-export const WEBPACK_STRUCTURE = 'ejected-webpack';
+export const WEBPACK_STRUCTURE = 'webpack';
 export const NORMAL_AND_SERVER = 'normal + another server folder project';
-export const WEBPACK_AND_SERVER = 'ejected-webpack + another server folder project';
+export const WEBPACK_AND_SERVER = 'webpack + another server folder project';
 export const MONOREPO = 'monorepo with the server and the client - not finished completely yet';
 export const MOBX = 'mobx';
-export const APOLLO = 'apollo js with graphQL';
+export const APOLLO = 'apollo with graphQL';
 
 export const functions = {
 	[NORAML_REACT]: normalReact,
