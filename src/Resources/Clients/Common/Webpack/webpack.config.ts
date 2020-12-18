406f51b20637a5b1660d0971a8ca6babfd24edca
Bar <baryed55@gmail.com>
2020-12-18 21:36:47 +0200

    nvm for now

--- src/Resources/Clients/Common/Webpack/webpack.config.ts
+++ src/Resources/Clients/Common/Webpack/webpack.config.ts
@@ -54,9 +54,6 @@ const config: webpack.Configuration = {
 				exclude: /node_modules/,
 				use: {
 					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env'],
-					},
 				},
 			},
 			{
